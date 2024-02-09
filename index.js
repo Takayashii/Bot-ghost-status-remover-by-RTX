dff04edf6747eee33beece55273d71be9f72fabf
Takayashii <159546828+Takayashii@users.noreply.github.com>
2024-02-09 14:34:29 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["In game arive.rip","Watching Tickets","Getting ELO on Arive"];
 
 
 let currentIndex = 0;
