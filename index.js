6e0f14f3787ad41d22f2ba1a2870a836ac5b2a04
ignTakayashii <159546828+Takayashii@users.noreply.github.com>
2024-04-08 15:39:23 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["In game arive.rip","Watching Tickets","Playing NoDebuff"];
+const statusMessages = ["In game VantagePvP","Watching Tickets","Playing NoDebuff"];
 
 
 let currentIndex = 0;
